afd0936e33fafda25805996890c755c8742fa8de
Parfunc Account <parfunc@googlegroups.com>
2016-07-28 10:41:55 -0400

    Attempt to install SC ghc 0.4 from the web

--- frameworks/Haskell/snap/setup.sh
+++ frameworks/Haskell/snap/setup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+set -x
+
 sed -i 's|host=".*"|host="'"${DBHOST}"'"|g' bench/cfg/db.cfg
 
 fw_depends stack
 
-cd bench
+DIR=`pwd`
 
+cd /tmp/
+wget http://cs.indiana.edu/~rrnewton/temp/ghc-sc-v0.4-opt.tgz
+tar xf ghc-sc-v0.4-opt.tgz
+mv ghc-sc-v0.4-opt /opt/ghc
 export PATH=/opt/ghc/bin:$PATH
+echo "Custom SC-GHC set up."
+
+cd $DIR/bench
+
 ${IROOT}/stack --allow-different-user build --system-ghc --no-install-ghc
 
 ${IROOT}/stack --allow-different-user exec snap-bench -- +RTS -A4M -N -qg2 -I0 -G2 &
