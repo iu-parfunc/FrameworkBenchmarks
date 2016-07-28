c880d39732b359988fd91717fc0310a9eb99f3ca
Parfunc Account <parfunc@googlegroups.com>
2016-07-28 17:09:11 -0400

    Add a sudo

--- frameworks/Haskell/snap/setup.sh
+++ frameworks/Haskell/snap/setup.sh
@@ -11,7 +11,7 @@ DIR=`pwd`
 cd /tmp/
 wget http://cs.indiana.edu/~rrnewton/temp/ghc-sc-v0.4-opt.tgz
 tar xf ghc-sc-v0.4-opt.tgz
-mv ghc-sc-v0.4-opt /opt/ghc
+sudo mv ghc-sc-v0.4-opt /opt/ghc
 export PATH=/opt/ghc/bin:$PATH
 echo "Custom SC-GHC set up."
 
