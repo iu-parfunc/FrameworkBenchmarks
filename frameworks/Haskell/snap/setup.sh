79eaca80914499230ea4b1b61a38af6d09187803
Ryan Newton <rrnewton@gmail.com>
2016-07-31 13:12:24 -0400

    Configure the other Haskell frameworks to install the SC variant of GHC

--- frameworks/Haskell/snap/setup.sh
+++ frameworks/Haskell/snap/setup.sh
@@ -6,16 +6,8 @@ sed -i 's|host=".*"|host="'"${DBHOST}"'"|g' bench/cfg/db.cfg
 
 fw_depends stack
 
-DIR=`pwd`
-
-cd /tmp/
-wget http://cs.indiana.edu/~rrnewton/temp/ghc-sc-v0.4-opt.tgz
-tar xf ghc-sc-v0.4-opt.tgz
-sudo mv ghc-sc-v0.4-opt /opt/ghc
-export PATH=/opt/ghc/bin:$PATH
-echo "Custom SC-GHC set up."
-
-cd $DIR/bench
+`dirname $0`/../setup_sc_ghc.sh
+cd ./bench
 
 ${IROOT}/stack --allow-different-user build --system-ghc --no-install-ghc
 
--- frameworks/Haskell/wai/setup.sh
+++ frameworks/Haskell/wai/setup.sh
@@ -2,8 +2,10 @@
 
 fw_depends stack
 
+`dirname $0`/../setup_sc_ghc.sh
+
 cd bench
 
-${IROOT}/stack --allow-different-user build --install-ghc
+${IROOT}/stack --allow-different-user build --system-ghc --no-install-ghc
 
 ${IROOT}/stack --allow-different-user exec bench -- ${MAX_THREADS} ${DBHOST} +RTS -A32m -N${MAX_THREADS} &
