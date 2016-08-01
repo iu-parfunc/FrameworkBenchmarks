3f80eb421c03e21c6db43dbac5de1647561e8cd6
Parfunc Account <parfunc@googlegroups.com>
2016-07-31 23:47:45 -0400

    Change other stack.yaml files to expect ghc 7.10.4

--- frameworks/Haskell/setup_sc_ghc.sh
+++ frameworks/Haskell/setup_sc_ghc.sh
@@ -5,7 +5,7 @@ set -xe
 DIR=`pwd`
 
 cd /tmp/
-wget http://cs.indiana.edu/~rrnewton/temp/ghc-sc-v0.4-opt.tgz
+wget -q http://cs.indiana.edu/~rrnewton/temp/ghc-sc-v0.4-opt.tgz
 tar xf ghc-sc-v0.4-opt.tgz
 sudo mv ghc-sc-v0.4-opt /opt/ghc
 export PATH=/opt/ghc/bin:$PATH
